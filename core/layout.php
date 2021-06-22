e52e9a84edc95b526cf68df27a128d3228cebee6
Yaritza-Aleman <Yrtz.aleman@yahoo.com>
2021-06-22 18:33:27 -0500

    added layout

--- core/layout.php
+++ core/layout.php
@@ -30,16 +30,50 @@ function active($name){
 
 <?php endif; ?>
 <!-- End sanitized content -->
-
-      <meta charset="UTF-8">
-
-      <title>About Yaritza A.</title>
+<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>About Yaritza A.</title>
+    
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css">
+  </head>
+  <body style="background-color: #5F9EA0"> 
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
       <link rel="stylesheet" type="text/css" href="css/dist/main.css">
   </head>
   <body>
   <body style="background-color: #D3D3D3"> 
-  <div class="row">
+  
+  <!-- Navbar content -->
+</nav>
+  <div class="row">      
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <nav class="navbar navbar-dark bg-dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="http://localhost/example.com/public/index.php">Home</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav">
+        <a class="nav-link active" aria-current="page" href="http://localhost/example.com/public/resume.php">Resume</a>
+        <a class="nav-link" href="http://localhost/example.com/public/contact.php">Contact</a>
+        <a class="nav-link" href="http://localhost/example.com/public/register.php">Register</a>
+        <a class="nav-link" href="http://localhost/example.com/public/users/index.php">Users</a>
+        <a class="nav-link disabled" href="http://localhost/example.com/public/posts/" tabindex="-1" aria-disabled="true">Post</a>
+      </div>
+    </div>
+  </div>
+</nav>
+<li class="nav-item">
+<?php if(!empty($_SESSION['user']['id'])): ?>
+<a class="nav-link" href="http://localhost/example.com/public/logout.php">Logout</a>
+ <?php else: ?>
+<a class="nav-link <?php echo active('login.php'); ?>" href="http://localhost/example.com/public/login.php">Login</a>
+  <?php endif; ?>
+   </li>
+   </div>
+            </div>
+            </nav>
             <div id="Content">
                 <?php echo $content; ?>
             </div>
@@ -49,45 +83,9 @@ function active($name){
                 <img class="avatar" src="https://s.gravatar.com/avatar/f8a45276bb23b3cd72fe7533b7bad4d2?s=80" alt="My Avatar" class="img-circle">
               </div>
             </div>
-        </div>
-
-    <div id="Wrapper">
-        <nav class="top-nav">
-            <!-- <a href="index.html" class="pull-left" href="/">"favicon-16x16.png"</a> -->
-            <div class="collapse navbar-collapse" id="navbarNavDropdown">
-        <ul class="navbar-nav">
-          <li class="nav-item active">
-            <a class="nav-link" href="http://localhost/example.com/public/index.php">Home</span></a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="http://localhost/example.com/public/resume.php">Resume</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="http://localhost/example.com/public/contact.php">Contact</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="http://localhost/example.com/public/register.php">Register</a>
-          </li>
-              <li class="nav-item">
-            <a class="nav-link" href="http://localhost/example.com/public/posts/">Post</a>
-          </li> 
-            
-            <li class="nav-item">
-            <a class="nav-link" href="http://localhost/example.com/public/users/index.php">Users</a>
-          </li>
-          
-           <li class="nav-item">
-            <a class="nav-link" href="http://localhost/example.com/public/login.php">Login</a>
-          </li> 
-          
-          <li class="nav-item">
-            <a class="nav-link" href="http://localhost/example.com/public/logout.php">Logout</a>
-          </li>
-          </a>
-
-
+  
         
-        <div id="Footer" class="clearfix">
+            <div id="Footer" class="clearfix">
             <small>&copy; 2021 - MyAwesomeSite.com</small>
             <ul role="navigation">
                 <li><a href="terms.html">Terms</a></li>
